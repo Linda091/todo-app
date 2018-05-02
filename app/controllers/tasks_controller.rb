2b3af0d32648dcec014dee3f9701f483453355e4
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-05-02 15:10:47 +0000

    supported the ability to update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,19 @@ class TasksController < ApplicationController
   def index
     render json: Task.all
   end
+
+  def update
+    task = Task.find(params[:id])              #find the task first
+    task.update_attributes(task_params)        #update the task attributes, specifically the parameters
+    render json: task                          # after you update the task, render the task in json
+  end
+
+
+private
+
+  def task_params
+    params.require(:task).permit(:done)
+  end
+
+
 end
